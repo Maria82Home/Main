67e7d555b196b120e4328276fee6b1ff1fd60b88
Maria <home82@list.ru>
2023-06-18 18:19:37 +0500

    Algorithms 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,10 @@
-//import List1;
 public class Main {
     public static void main(String[] args) {
         List1 list=new List1();
-//        list.value=5;
+
 
         list.printClassName(list);
-//        list.pushFront(5);
-//        list.pushFront(7);
-//        list.pushFront(9);
-//        list.pushFront(11);
-//        list.pushFront(13);
+
         for(int i=100; i>=1; i--)
             list.pushFront(i);
 
